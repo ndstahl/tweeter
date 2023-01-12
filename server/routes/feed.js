fee9fb6d015d4b7edc9dcf521bcf01996561ef38
ndstahl <110428829+ndstahl@users.noreply.github.com>
2023-01-12 18:43:50 -0500

    modified the feed.js and home.component

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -2,11 +2,55 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/", async (req, res) => {
-  const feeds = await axios.get(
-    "https://mpb-site.s3.us-east-2.amazonaws.com/tweeter.json"
-  );
-  res.json(feeds.data);
+let feeds = [];
+
+// Create
+router.post("/", async (req, res) => {
+  const newTweet = {
+    name: "Nick Stahl",
+    handler: "@dev_Nick",
+    title: req.body.tweet,
+    profileImg: "",
+    tweetImg: "",
+  };
+  feeds.push(newTweet);
+  res.json(feeds);
 });
 
+// Update
+router.put("/:id", async (req, res) => {
+  const {
+    params: { id },
+    body: { tweet },
+  } = req;
+
+  feeds[id] = {
+    name: "Nick Stahl",
+    handler: "@dev_Nick",
+    title: req.body.tweet,
+    profileImg: "",
+    tweetImg: "",
+  };
+
+  res.json({
+    msg: "Tweet Updated",
+    data: feeds,
+  });
+});
+
+// Read
+router.get("/", async (req, res) => {
+  const tweeterData = await axios.get(
+    "https://mpb-site.s3.us-east-2.amazonaws.com/tweeter.json"
+  );
+  feeds = tweeterData.data;
+  res.json(tweeterData.data);
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  res.json({
+    msg: "Tweet has been deleted",
+  });
+});
 module.exports = router;
